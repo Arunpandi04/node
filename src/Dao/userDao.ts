0529562949a082bde51300a7df994f24546cbe4b
Arunpandi10d <arunpandi@10decoders.in>
2022-01-06 15:58:03 +0530

    redueces

--- src/Dao/userDao.ts
+++ src/Dao/userDao.ts
@@ -1,12 +1,8 @@
 import userModal from '../Modal/userModal';
 import {signupData} from '../Utils/Types'
-// import { getRepository,getConnection } from "typeorm";
-// import User from '../entity/user.entity'
-// import Address from '../entity/address.entity';
-// import post from '../entity/post.entity'
+
 export class userDao {
     public async create_cart(body:any){
-        // let connection = await getConnection();
         const result = new userModal({
             firstName: body.firstName,
             lastName: body.lastName,
@@ -16,27 +12,16 @@ export class userDao {
             email:body.email,
             password:body.password
           });
-
-        //   const users = new User();
-        //   const userRepo =connection.getRepository(User)
-        //   return userRepo.save(body)
         return result.save()
     }
 
     public async getUser(id:string):Promise<any>{
-        // let connection = await getConnection();
-        // const userRepo =connection.getRepository(User)
-        // return await userRepo.findOne({id},{ relations: ['posts'] }); 
         return userModal.findById(id)
     }
     public async findone(data:string){
         return await userModal.findOne({email:data}); 
     }
     public async updateuser(id:string,data:any):Promise<object | null>{
-        // let connection = await getConnection();
-        // const userRepo =connection.getRepository(User)
-    //    return await userRepo.update({id}, data);
-    // return await userModal.findByIdAndUpdate({})
     return await userModal.findOneAndUpdate({_id: id}, data);
     }
 }
